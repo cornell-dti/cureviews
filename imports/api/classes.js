86c25c200cdeed83f307512ef2959ab16e79fd40
Divyansha S <divyansha.sehgal@gmail.com>
2017-11-01 12:44:08 -0400

    commenting out the db build

--- imports/api/classes.js
+++ imports/api/classes.js
@@ -216,9 +216,9 @@ if (Meteor.isServer) {
     });
 
     // COMMENT THESE OUT AFTER THE FIRST METEOR BUILD!!
-    Classes.remove({});
-    Subjects.remove({});
-    addAllCourses(findAllSemesters());
+    //Classes.remove({});
+    //Subjects.remove({});
+    //addAllCourses(findAllSemesters());
 }
 
 //Other helper functions used above
