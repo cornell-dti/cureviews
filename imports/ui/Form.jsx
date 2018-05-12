dc07702ff79bf055b09780013f15a4d2d6512c2d
VinishaM <vm297@cornell.edu>
2018-05-12 02:23:34 -0400

    remove import

--- imports/ui/Form.jsx
+++ imports/ui/Form.jsx
@@ -1,5 +1,4 @@
 import React, { Component} from 'react';
-import { Checkbox } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import { Reviews } from '../api/dbDefs.js';
