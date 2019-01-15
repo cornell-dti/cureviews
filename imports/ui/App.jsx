f0b9e8d91a7f02bea4fa4439ead02893d0d7aa5f
Julian Londono <jlond10@gmail.com>
2019-01-15 02:23:13 -0500

    Fix search results alignment

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -57,7 +57,7 @@ export default class App extends Component {
             <img src='/logo.svg' className="img-responsive center-block" id="img-padding-top" alt="" />
         </div>
         <div className="row">
-          <div className="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+          <div className="col-md-9 col-sm-9 col-xs-9 center-block no-float">
             <SearchBar query={this.state.query} queryFunc={this.updateQuery} />
           </div>
         </div>
