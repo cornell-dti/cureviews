68ae5179aa6316bf9c85c5724a2e4f8fb3a993da
yc686 <yc686@cornell.edu>
2019-04-22 20:59:27 -0400

    added tokens for resetProfessor & updateProfessor

--- server/methods.js
+++ server/methods.js
@@ -17,13 +17,13 @@ Meteor.methods({
   // Upon success returns 1, else returns 0.
   insert: function (token, review, classId) {
     // check: only insert if all form fields are filled in
-    if(token == undefined){
+    if (token == undefined) {
       console.log("Token was undefined in insert")
       return 0; // Token was undefined
     }
     const ticket = Meteor.call('getVerificationTicket', token);
     const insertUserCall = Meteor.call('insertUser', ticket);
-    if (ticket.hd === "cornell.edu"){
+    if (ticket.hd === "cornell.edu") {
       if (review.text !== null && review.diff !== null && review.rating !== null && review.workload !== null && review.professors !== null && classId !== undefined && classId !== null && review.memberReferral !== null) {
         var fullReview = {
           text: review.text,
@@ -53,11 +53,11 @@ Meteor.methods({
         console.log("Some review values are null")
         return 0; //fail
       }
-    } else{
+    } else {
       console.log("Error: non-Cornell email attempted to insert review")
       return 0;
     }
-    
+
   },
 
   //Inserts a new user into the Users collection.
@@ -65,8 +65,8 @@ Meteor.methods({
   insertUser: function (googleObject) {
     //Check user object has all required fields
     if (googleObject.given_name != null
-          && googleObject.family_name != null
-          && googleObject.email.replace("@cornell.edu", "") != null) {
+      && googleObject.family_name != null
+      && googleObject.email.replace("@cornell.edu", "") != null) {
       var newUser = {
         firstName: googleObject.given_name,
         lastName: googleObject.family_name,
@@ -77,7 +77,7 @@ Meteor.methods({
       };
 
       const user = Meteor.call('getUserByNetId', googleObject.email.replace("@cornell.edu", ""));
-      if(user == null){
+      if (user == null) {
         try {
           //check(newUser, Users);
           Students.insert(newUser);
@@ -88,7 +88,7 @@ Meteor.methods({
         }
       }
       return 1; //No need to add user again
-      
+
 
     }
     else {
@@ -166,7 +166,7 @@ Meteor.methods({
   // Should be called by an admin via the admin page once a semester.
   // TODO uncomment
   // addNewSemester: function (initiate, token) {
-    // const userIsAdmin = Meteor.call('tokenIsAdmin', token);
+  // const userIsAdmin = Meteor.call('tokenIsAdmin', token);
   //   // ensure code is running on the server, not client
   //   if (initiate && Meteor.isServer && userIsAdmin) {
   //     console.log("updating new semester");
@@ -219,8 +219,9 @@ Meteor.methods({
   NOTE: We are temporarily not updating any professors for classes inspect
   'SU14','SU15','SU16','SU17','SU18', 'FA18', 'WI18'*/
   // TODO uncomment
-  // setProfessors: function (initiate) {
-  //   if (initiate && Meteor.isServer) {
+  // setProfessors: function (initiate, token) {
+  // const userIsAdmin = Meteor.call('tokenIsAdmin', token);
+  //   if (initiate && Meteor.isServer && userIsAdmin) {
   //     var semesters = findAllSemesters();
   //     // var toRemove = ['SU14','SU15','SU16','SU17','WI14','WI15','WI16','WI17','SU18', 'FA18', 'WI18']
   //     var toRemove = ['SU18', 'FA18', 'WI18']
@@ -248,8 +249,9 @@ Meteor.methods({
   NOTE: We are temporarily not updating any professors for classes inspect
   'SU18', 'FA18', 'WI18'*/
   // TODO uncomment
-  // resetProfessors: function (initiate) {
-  //   if (initiate && Meteor.isServer) {
+  // resetProfessors: function (initiate, token) {
+  // const userIsAdmin = Meteor.call('tokenIsAdmin', token);
+  //   if (initiate && Meteor.isServer && userIsAdmin) {
   //     var semesters = findAllSemesters();
   //     var toRemove = ['SU18', 'FA18', 'WI18']
   //     toRemove.forEach(function (sem) {
@@ -283,16 +285,16 @@ Meteor.methods({
     }
     return null;
   },
-  
+
   //Returns true if user matching "netId" is an admin
   tokenIsAdmin: function (token) {
     // console.log("This is token in tokenIsAdmin");
     // console.log(token);
-    if (token != undefined){
+    if (token != undefined) {
       const ticket = Meteor.call('getVerificationTicket', token);
       // console.log(ticket);
       const user = Meteor.call('getUserByNetId', ticket.email.replace("@cornell.edu", ""));
-      if (user){
+      if (user) {
         return user.privilege === "admin";
       }
     }
@@ -340,7 +342,7 @@ Meteor.methods({
   // Un-flag a review, making it visible to everyone and "unreported"
   // To be called by an admin via the admin interface.
   undoReportReview: function (review, token) {
-    const userIsAdmin=Meteor.call('tokenIsAdmin', token);
+    const userIsAdmin = Meteor.call('tokenIsAdmin', token);
     // check: make sure review id is valid and non-malicious
     var regex = new RegExp(/^(?=.*[A-Z0-9])/i)
     if (regex.test(review._id) && userIsAdmin) {
@@ -432,7 +434,7 @@ Meteor.methods({
    */
   getVerificationTicket: async function (token) {
     try {
-      if(token == undefined){
+      if (token == undefined) {
         console.log("Token was undefined in getVerificationTicket")
         return 0; // Token was undefined
       }
@@ -456,14 +458,14 @@ Meteor.methods({
       // console.log(emailBeforeAt);
       // console.log(netid);
       const valid_email = emailBeforeAt == netid;
-      
+
       return valid_email;
 
     } catch (e) {
       console.log(e);
       return false;
     }
-    
+
   },
   /**
    * Used in the .catch when verify is used, handles whatever should be done
