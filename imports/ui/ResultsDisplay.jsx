20362c828c2240a0ab83c05c115443d039ac68d7
jjc387 <jjc387@cornell.edu>
2019-11-22 19:25:44 -0500

    sorting function works

--- imports/ui/ResultsDisplay.jsx
+++ imports/ui/ResultsDisplay.jsx
@@ -25,13 +25,39 @@ export default class ResultsDisplay extends Component {
       courseList: this.props.courses,
       card_course: this.props.courses[0],
       active_card: 0,
+      selected: "rating"
     };
 
     this.previewHandler = this.previewHandler.bind(this);
+    this.sortDesc = this.sort.bind(this);
+
+  }
+
+  handleSelect = (event) => {
+    var opt = event.target.value;
+    this.setState({ selected: opt }, () => this.sort());
+
+  }
+
+  sort() {
+    if (this.state.selected == "rating") {
+      var data = this.state.courseList.sort((a, b) => (b.classRating - a.classRating));
+      this.setState({ courseList: data });
+    }
+    else if (this.state.selected == "diff") {
+      var data = this.state.courseList.sort((a, b) => (a.classDifficulty - b.classDifficulty));
+      this.setState({ courseLIst: data });
+
+    }
+    else if (this.state.selected == "work") {
+      var data = this.state.courseList.sort((a, b) => ((a.classWorkload == null ? Number.MAX_SAFE_INTEGER : a.classWorkload) - (b.classWorkload == null ? Number.MAX_SAFE_INTEGER : b.classWorkload)));
+      this.setState({ courseList: data });
+    }
 
   }
 
   renderResults() {
+    console.log("current state " + this.state.courseList[0].classDifficulty);
     return this.state.courseList.map((result, index) => (
       <FilteredResult key={index} index={index} border_color={index == this.state.active_card ? "solid 1px #4a90e2" : "solid 0.5px #d8d8d8"} course={result} previewHandler={this.previewHandler} />
     ));
@@ -49,8 +75,15 @@ export default class ResultsDisplay extends Component {
   render() {
     return (
       <div className="row">
-        <div className="col-md-2 col-sm-2 col-xs-2">
+        <div className="col-md-1 col-sm-1 col-xs-1">
           Filters
+        </div>
+        <div className="col-md-1 col-sm-1 col-xs-1">
+          <select className="browser-default custom-select" onChange={(e) => this.handleSelect(e)}>
+            <option value="rating">Overall Rating</option>
+            <option value="diff" >Difficulty</option>
+            <option value="work">Workload</option>
+          </select>
         </div>
         <div className="col-md-5 col-sm-5 col-xs-5" id="results">
           <ul>
