add93cc7150bbbb8bbb70035e08a4df318f8718f
Julian Londono <jlond10@gmail.com>
2020-01-11 11:40:58 -0500

    Add support for access-control-allow-origin header in API

--- imports/api/dbDefs.js
+++ imports/api/dbDefs.js
@@ -9,7 +9,14 @@ try {
   console.log(e);
 }
 
-  
+JsonRoutes.setResponseHeaders({
+  "Cache-Control": "no-store",
+  "Pragma": "no-cache",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+});  
+
 /*
 
  Database definitions file. Defines all collections in the local database,
