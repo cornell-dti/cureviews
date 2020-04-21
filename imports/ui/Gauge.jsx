aafeb4bee5065f2e7629bb6296e0197eb68e40a5
Julian Londono <jlond10@gmail.com>
2020-04-21 03:45:51 -0400

    Finalize gauge responsiveness

--- imports/ui/Gauge.jsx
+++ imports/ui/Gauge.jsx
@@ -16,6 +16,8 @@ export default class Gauge extends Component{
     this.state=initState;
     this.updateRating=this.updateRating.bind(this);
     this.componentDidUpdate=this.componentDidUpdate.bind(this);
+    this.gauge_text_top = props.isInPreviewCard ? "gauge-text-top-preview" : "gauge-text-top";
+
   }
 
     componentDidMount(){
@@ -32,7 +34,7 @@ export default class Gauge extends Component{
       if(!isNaN(this.props.rating)){
         let percentage = 20*this.props.rating;
         let color= "hsl(212, 100%,"+(86-(percentage*.36))+"%)";
-        this.setState({percentage: percentage, color: color, rating: this.props.rating});
+        this.setState({percentage: percentage, color: color, rating: this.props.rating.toFixed(1)});
       }
       else{
         this.setState({rating: "-"});
@@ -51,15 +53,15 @@ export default class Gauge extends Component{
     return (
 
         <div className="row noLeftRightMargin gauge-component-container">
-          <div className="col-md-6 col-md-offset-1 no-side-padding gauge-text">
-            <h1 className="gauge-text-top">
+          <div className="col-md-6 no-side-padding gauge-text">
+            <h1 className={this.gauge_text_top}>
                 {this.state.rating}
             </h1>
             <h1 className="gauge-text-bottom">
               {this.props.text}
             </h1>
           </div>
-          <div className="col-md-5 no-side-padding gauge-height-center">
+          <div className="col-md-6 no-side-padding gauge-height-center">
             <div className="gauge-container">
               <CircularProgressbar value={this.state.percentage} strokeWidth={10} styles={buildStyles({ pathColor: this.state.color, strokeLinecap: "butt" })}/>
             </div>
@@ -74,5 +76,6 @@ Gauge.propTypes ={
   width: PropTypes.string.isRequired,
   height: PropTypes.string.isRequired,
   rating: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  isInPreviewCard: PropTypes.bool
 };
