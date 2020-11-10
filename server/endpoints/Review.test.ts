4d860f8d69352abe6267e1f5e90b49a47d5206b4
Akash Aryal <aryal.akash4414@gmail.com>
2020-11-10 09:27:31 -0600

    removed logs

--- server/endpoints/Review.test.ts
+++ server/endpoints/Review.test.ts
@@ -72,55 +72,6 @@ beforeAll(async () => {
   await Promise.all(
     testReviews.map(async (r) => await (new Reviews(r).save())),
   );
-  /* const c2110 = new Classes({
-    _id: "oH37S3mJ4eAsktypy",
-    classSub: "cs",
-    classNum: "2110",
-    classTitle: "Object-Oriented Programming and Data Structures",
-    classPrereq: [],
-    classFull: "cs 2110 object-oriented programming and data structures",
-    classSems: ["FA14", "SP15", "SU15", "FA15", "SP16", "SU16", "FA16", "SP17",
-      "SU17", "FA17", "SP18", "FA18", "SU18", "SP19", "FA19", "SU19"],
-    crossList: ["q75SxmqkTFEfaJwZ3"],
-    classProfessors: ["David Gries", "Douglas James", "Siddhartha Chaudhuri",
-      "Graeme Bailey", "John Foster", "Ross Tate", "Michael George",
-      "Eleanor Birrell", "Adrian Sampson", "Natacha Crooks", "Anne Bracy",
-      "Michael Clarkson"],
-    classDifficulty: 2.9,
-    classRating: null,
-    classWorkload: 3,
-  });
-
-  const review1 = new Reviews({
-    _id: "4Y8k7DnX3PLNdwRPr",
-    text: "review text for cs 2110",
-    difficulty: 1,
-    quality: 4,
-    class: "oH37S3mJ4eAsktypy",
-    grade: 6,
-    date: new Date().toISOString(),
-    atten: 0,
-    visible: 1,
-    reported: 0,
-  });
-
-  const review2 = new Reviews({
-    _id: "4Y8k7DnX3PLNdwRPq",
-    text: "review text for cs 2110 number 2",
-    difficulty: 1,
-    quality: 5,
-    class: "oH37S3mJ4eAsktypy",
-    grade: 6,
-    date: new Date().toISOString(),
-    atten: 0,
-    visible: 1,
-    reported: 0,
-  });
-
-  await review2.save();
-  await review1.save();
-  await c2110.save();
-  */
 
   // Set up a mock version of the v2 endpoints to test against
   const app = express();
