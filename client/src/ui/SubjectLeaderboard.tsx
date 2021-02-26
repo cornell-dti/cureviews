a63244596a363436f824011804458d121c3b4068
Akash Aryal <aryal.akash4414@gmail.com>
2021-02-25 19:49:39 -0600

    updated topSubjects to use token

--- client/src/ui/SubjectLeaderboard.tsx
+++ client/src/ui/SubjectLeaderboard.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Meteor } from '../meteor-shim';
 import axios from 'axios';
+import { Session } from '../meteor-session';
 
 type State = { readonly topSubjects: readonly any[] };
 
@@ -19,7 +20,7 @@ export default class SubjectLeaderboard extends Component<{}, State> {
   componentDidMount() {
     // get the top subjects by number of reviews, using a Meteor function
     // defined in imports/api/classes
-    axios.post(`/v2/topSubjects`).then((res) => {
+    axios.post(`/v2/topSubjects`, { token: Session.get("token") }).then((res) => {
       let data = res.data.result;
       this.setState({ topSubjects: data });
     }).catch((err) => {
