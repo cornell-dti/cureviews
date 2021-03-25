1f78a0afb0d23bd4bc28a351cb3b2fbf8c1f1723
Tanay Menezes <tm452@cornell.edu>
2021-03-25 18:51:42 -0400

    Implemented homepage background change based on time of day and season

--- client/src/ui/App.tsx
+++ client/src/ui/App.tsx
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar.jsx';
 import "./css/App.css";
 
+var date = new Date();
+var month = date.getMonth();
+var hours = date.getHours();
+var monthclass = "";
+var dayclass = "afternoon";
+
+if (month < 2 || month > 10) {
+  monthclass = "winter"
+}
+
+else if (month > 6) {
+  monthclass = "fall"
+}
+
+if (hours < 5 || hours > 19) {
+  dayclass = "night"
+  monthclass = "";
+}
+
+else if (hours > 17) {
+  dayclass = "sunset"
+}
+
 /*
   App Component. Uppermost View component in the component tree,
   the first element of the HTML body tag grabbed by main.html.
@@ -12,7 +35,7 @@ import "./css/App.css";
 export default class App extends Component {
   render() {
     return (
-      <div className="container-fluid full-height background-gradient">
+      <div className={"container-fluid full-height background-gradient_" + dayclass + monthclass}>
         <div className="row">
           <img src='/logo.svg' className="img-responsive scale-logo-homepage" alt="CU Reviews Logo" />
         </div>
