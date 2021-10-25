683ab339c3e17e801aeedb1e02360d8a2ab33159
Akash Aryal <aryal.akash4414@gmail.com>
2021-10-25 17:33:10 -0400

    added comments

--- server/endpoints/TestServer.ts
+++ server/endpoints/TestServer.ts
@@ -2,30 +2,31 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from 'mongoose';
 import express from "express";
 import { Student, Class, Professor, Review, Subject } from 'common';
+import * as http from "http";
 import { Classes, Students, Subjects, Professors, Reviews } from "../dbDefs";
 import { configure } from "../endpoints";
 
 export const testingPort = 8080;
 
+/**
+ * This class is sets up the express endpoint and mongo memory server for testing.
+ */
 export default class TestingServer {
   private mongoServer: MongoMemoryServer;
 
-  private serverCloseHandle;
-
-  private port: number;
+  private serverCloseHandle: http.Server;
 
   constructor(port) {
-    this.port = port;
     this.mongoServer = new MongoMemoryServer();
+    // setup express
     const app = express();
     this.serverCloseHandle = app.listen(port);
     configure(app);
   }
 
-  getPort = () => this.port
-
   setUpDB = async (reviews: Review[] = [], students: Student[] = [],
     classes: Class[] = [], professors: Professor[] = [], subjects: Subject[] = []) => {
+    // setup db
     const mongoUri = await this.mongoServer.getUri();
     await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -33,6 +34,7 @@ export default class TestingServer {
     await mongoose.connection.collections.subjects.createIndex({ subShort: "text" });
     await mongoose.connection.collections.professors.createIndex({ fullName: "text" });
 
+    // add classes, reviews, etc... to db collections
     await Promise.all(
       reviews.map(async (c) => await (new Reviews(c).save())),
     );
@@ -54,20 +56,13 @@ export default class TestingServer {
     );
   }
 
-  emptyDB = async () => {
-    const collections = await mongoose.connection.db.collections();
-    await Promise.all(
-      collections.map((c) => c.deleteMany({})),
-    );
-  }
-
   shutdownTestingServer = async () => {
     await mongoose.disconnect();
     await this.mongoServer.stop();
+    // serverCloseHandle becomes undefined sometimes for whatever reason so we
+    // enclose in if statement
     if (this.serverCloseHandle !== undefined) {
       this.serverCloseHandle.close();
     }
-    this.mongoServer = null;
-    this.serverCloseHandle = null;
   }
 }
