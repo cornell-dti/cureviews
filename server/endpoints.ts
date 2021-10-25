ba590081a45cbc2c3b28f64d4b4422023691cf62
Akash Aryal <aryal.akash4414@gmail.com>
2021-10-25 08:29:46 -0400

    revert pushes back

--- server/endpoints.ts
+++ server/endpoints.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { validationResult, ValidationChain } from "express-validator";
 import { totalReviews, howManyReviewsEachClass, howManyEachClass, topSubjects, getReviewsOverTimeTop15 } from "./endpoints/AdminChart";
 import { getReviewsByCourseId, getCourseById, insertReview, insertUser, getCourseByInfo, incrementLike, decrementLike } from "./endpoints/Review";
-import { getTotalLikesByStudentId } from "./endpoints/Profile";
 import { tokenIsAdmin } from "./endpoints/Auth";
 import { getCoursesByProfessor, getCoursesByMajor, getClassesByQuery, getSubjectsByQuery, getProfessorsByQuery } from "./endpoints/Search";
 import { fetchReviewableClasses, reportReview, makeReviewVisible, undoReportReview, removeReview } from "./endpoints/AdminActions";
@@ -54,7 +53,6 @@ export function configure(app: express.Application) {
   register(app, "getReviewsOverTimeTop15", getReviewsOverTimeTop15);
   register(app, "incrementLike", incrementLike);
   register(app, "decrementLike", decrementLike);
-  register(app, "getTotalLikesByStudentId", getTotalLikesByStudentId);
 }
 
 function register<T>(app: express.Application, name: string, endpoint: Endpoint<T>) {
