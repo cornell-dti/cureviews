2a02fed3161391cd8056e1c9aa77ce61307ad3e3
Michelle Li <michelleyli123@gmail.com>
2021-11-01 12:42:11 -0400

    set up left profile card

--- client/src/ui/Profile.tsx
+++ client/src/ui/Profile.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
-import ProfileCard from './ProfileCard'
+import React from "react";
+import ProfileCard from "./ProfileCard";
 
-export default function Profile() {
+import styles from "./css/Profile.module.css";
+
+type ProfileProps = {
+  imageSrc: any;
+  verifiedEmail: string;
+};
+
+export default function Profile({
+  imageSrc = "/profile_bear.png",
+  verifiedEmail = "myl39@cornell.edu",
+}: ProfileProps) {
   return (
-    <div>
-      <h2>Profile</h2>
-      <ProfileCard title="Reviews Total" value="12" image="/total_reviews_icon.svg"></ProfileCard>
-      <ProfileCard title="People found your reviews helpful" value="7" image="/helpful_review_icon.svg"></ProfileCard>
-      <ProfileCard title="Views Total" value="12" image="/views_icon.svg"></ProfileCard>
-
+    <div className={styles.profileContainer}>
+      <div className={styles.profileTitle}>Profile</div>
+      <div className={styles.profileInfo}>
+        <img src={imageSrc} alt='user' />
+        <div className={styles.profileVerifiedEmail}>
+          Verified as: {verifiedEmail}
+        </div>
+        <div className={styles.profileUserStatisticsText}>User Statistics</div>
+        <div className={styles.profileUserStatistics}>
+          <ProfileCard
+            title='Reviews Total'
+            value='12'
+            image='/total_reviews_icon.svg'
+          ></ProfileCard>
+          <ProfileCard
+            title='People found your reviews helpful'
+            value='7'
+            image='/helpful_review_icon.svg'
+          ></ProfileCard>
+          <ProfileCard
+            title='Views Total'
+            value='12'
+            image='/views_icon.svg'
+          ></ProfileCard>
+        </div>
+        <button className={styles.profileSignOutButton}>
+          <p className={styles.profileSignOutText}>Signout</p>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
