edc9cf4990e5319672dd928c0adc7ec19b750091
jack-farley <jfarley872@gmail.com>
2021-11-02 21:58:40 -0400

    working on login popup

--- client/src/ui/LoginModal.tsx
+++ client/src/ui/LoginModal.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CUreviewsGoogleLogin from './CUreviewsGoogleLogin';
 import './css/LoginModal.css'
 
 const LoginModal = () => {
+
+  const [executeLogin, setExecuteLogin] = useState(false);
   
   return (
     <div className="modal fade loginModal" id="signinModal" tabIndex={-1} role="dialog" aria-labelledby="signinModalLabel" aria-hidden="true">
       <div className="modal-dialog" role="document">
         <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="signinModalLabel">Modal title</h5>
+          <div className="modal-body">
             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
-          </div>
-          <div className="modal-body">
+            <div className="signin-cureviews-logo-container">
+              <img src='/logo.svg' className="signin-cureviews-logo" alt="CU Reviews Logo" />
+            </div>
             <div className="signin-message">
               <p>Don't worry - all of your reviews will be <b>anonymous!</b></p>
             </div>
             <div className="signin-button-container">
-              <button type="button" className="btn btn-primary">Verify Cornell Email</button>
+              <button type="button" className="btn btn-primary signin-button" onClick={() => (setExecuteLogin(true))}>Verify Cornell Email</button>
             </div>
-            <div className="signin-logo-container">
-              <img src='/dti_logo.png' className="signing-dti-logo" alt="DTI Logo" />
+            <div className="signin-bottom-message">
+              <p>You will be redirected to our login page. Not seeing it? <a href="https://www.google.com/">Click here</a></p>
             </div>
+            <div className="signin-dti-logo-container">
+              <img src='/dti-logo-grey.png' className="signing-dti-logo" alt="DTI Logo" />
+            </div>
+            <CUreviewsGoogleLogin
+                  executeLogin={executeLogin}
+                  waitTime={1500}
+                  redirectFrom="admin"/>
           </div>
         </div>
       </div>
