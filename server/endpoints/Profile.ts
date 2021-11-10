48e0ff74c73b5e44a6f6a520f6c170ea2d28e2dd
juliejeong <julie.seoyeon.jeong@gmail.com>
2021-11-09 20:28:35 -0500

    saving progress

--- server/endpoints/Profile.ts
+++ server/endpoints/Profile.ts
@@ -31,3 +31,24 @@ export const countReviewsByStudentId: Endpoint<NetIdQuery> = {
     }
   },
 };
+
+export const getReviewsByStudentId: Endpoint<NetIdQuery> = {
+  guard: [body("netId").notEmpty().isAscii()],
+  callback: async (ctx: Context, request: NetIdQuery) => {
+    const { netId } = request;
+    try {
+      const student = await Students.findOne({ netId });
+      if (student.reviews == null) {
+        return { code: 500, message: "No reviews object were associated." };
+      }
+
+      return { code: 200, message: student.reviews };
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log("Error: at 'getReviewsByStudentId' method");
+      // eslint-disable-next-line no-console
+      console.log(error);
+      return { code: 500, message: error.message };
+    }
+  },
+};
