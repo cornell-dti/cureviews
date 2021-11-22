5443f75253d535aeaca08b0b92e45f2f4e84ccb0
Michelle Li <michelleyli123@gmail.com>
2021-11-22 11:37:42 -0500

    added watched classes route

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,11 +4,12 @@ import "./index.css";
 import React from "react";
 import { render } from "react-dom";
 
-import Login from './ui/Login';
-import ClassView from './ui/ClassView.jsx';
-import App from './ui/App';
-import AuthRedirect from './ui/AuthRedirect';
-import Profile from './ui/Profile';
+import Login from "./ui/Login";
+import ClassView from "./ui/ClassView.jsx";
+import App from "./ui/App";
+import AuthRedirect from "./ui/AuthRedirect";
+import Profile from "./ui/Profile";
+import WatchedClasses from "./ui/WatchedClasses";
 
 import { BrowserRouter, Route } from "react-router-dom";
 import { Results } from "./ui/Results";
@@ -22,15 +23,32 @@ application component the user should see based on the URL they enter.
 
 */
 render(
-    <BrowserRouter>
-        <div className="container-fluid full-height noLeftRightPadding">
-            <Route name="app" exact path="/" component={App} />
-            <Route name="admin" exact path="/admin" component={Login} />
-            <Route name="permalink" exact path="/course/:subject/:number" component={ClassView} />
-            <Route name="auth" exact path="/auth" component={AuthRedirect} />
-            <Route name="permalink" exact path="/results/:type/:input" component={Results} />
-            <Route name="profile" exact path="/profile" component={Profile} />
-        </div>
-    </BrowserRouter>,
-    document.getElementById('render-target')
+  <BrowserRouter>
+    <div className='container-fluid full-height noLeftRightPadding'>
+      <Route name='app' exact path='/' component={App} />
+      <Route name='admin' exact path='/admin' component={Login} />
+      <Route
+        name='permalink'
+        exact
+        path='/course/:subject/:number'
+        component={ClassView}
+      />
+      <Route name='auth' exact path='/auth' component={AuthRedirect} />
+      <Route
+        name='permalink'
+        exact
+        path='/results/:type/:input'
+        component={Results}
+      />
+      {/* implement private route */}
+      <Route name='profile' exact path='/profile' component={Profile} />
+      <Route
+        name='watchedClasses'
+        exact
+        path='/classes'
+        component={WatchedClasses}
+      />
+    </div>
+  </BrowserRouter>,
+  document.getElementById("render-target"),
 );
