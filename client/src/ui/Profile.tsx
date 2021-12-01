e57a2b5209de2db0f484ae37ee6c1263fbf807d7
jack-farley <jfarley872@gmail.com>
2021-11-30 21:52:17 -0500

    adding navbar to my reviews page

--- client/src/ui/Profile.tsx
+++ client/src/ui/Profile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProfileCard from "./ProfileCard";
+import Navbar from './Navbar';
 
 import styles from "./css/Profile.module.css";
 
@@ -13,34 +14,37 @@ export default function Profile({
   verifiedEmail = "myl39@cornell.edu",
 }: ProfileProps) {
   return (
-    <div className={styles.profileContainer}>
-      <div className={styles.profileTitle}>Profile</div>
-      <div className={styles.profileInfo}>
-        <img src={imageSrc} alt='user' />
-        <div className={styles.profileVerifiedEmail}>
-          Verified as: {verifiedEmail}
+    <div>
+      <Navbar userInput = {""}></Navbar>
+      <div className={styles.profileContainer}>
+        <div className={styles.profileTitle}>Profile</div>
+        <div className={styles.profileInfo}>
+          <img src={imageSrc} alt='user' />
+          <div className={styles.profileVerifiedEmail}>
+            Verified as: {verifiedEmail}
+          </div>
+          <div className={styles.profileUserStatisticsText}>User Statistics</div>
+          <div className={styles.profileUserStatistics}>
+            <ProfileCard
+              title='Reviews Total'
+              value='12'
+              image='/total_reviews_icon.svg'
+            />
+            <ProfileCard
+              title='People found your reviews helpful'
+              value='7'
+              image='/helpful_review_icon.svg'
+            ></ProfileCard>
+            <ProfileCard
+              title='Views Total'
+              value='12'
+              image='/views_icon.svg'
+            ></ProfileCard>
+          </div>
+          <button className={styles.profileSignOutButton}>
+            <p className={styles.profileSignOutText}>Signout</p>
+          </button>
         </div>
-        <div className={styles.profileUserStatisticsText}>User Statistics</div>
-        <div className={styles.profileUserStatistics}>
-          <ProfileCard
-            title='Reviews Total'
-            value='12'
-            image='/total_reviews_icon.svg'
-          />
-          <ProfileCard
-            title='People found your reviews helpful'
-            value='7'
-            image='/helpful_review_icon.svg'
-          ></ProfileCard>
-          <ProfileCard
-            title='Views Total'
-            value='12'
-            image='/views_icon.svg'
-          ></ProfileCard>
-        </div>
-        <button className={styles.profileSignOutButton}>
-          <p className={styles.profileSignOutText}>Signout</p>
-        </button>
       </div>
     </div>
   );
