351c94102689f6fdbcc5e35db120eb5981c7aace
Andrew <kungpaogao@gmail.com>
2022-02-03 21:50:30 -0500

    Fix centering of CUReviews logo in auth redirect
    - update to use Bootstrap 4 img-fluid
    - set horizontal margin to auto

--- client/src/ui/ModalContentAuth.tsx
+++ client/src/ui/ModalContentAuth.tsx
@@ -5,12 +5,9 @@ import styles from "./css/ModalContentAuth.module.css";
 export default function ModalContentAuth() {
   return (
     <>
-      <img
-        src="/logo.svg"
-        className={`img-responsive center-block ${styles.logo}`}
-        id="img-padding-top"
-        alt="CU Reviews Logo"
-      />
+      <div className={styles.logo}>
+        <img src="/logo.svg" className="img-fluid" alt="CU Reviews Logo" />
+      </div>
       <p className={styles.title}>You're almost there!</p>
       <p className={styles.text}>
         Log in with your <code>cornell.edu</code> account to verify you are a
