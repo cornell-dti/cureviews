e48b2be9e26ed29339e27d344ed66f4ebdc733b2
Michelle Li <michelleyli123@gmail.com>
2022-03-03 18:18:40 -0500

    gets cornell email from backend

--- client/src/ui/Profile.tsx
+++ client/src/ui/Profile.tsx
@@ -1,41 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileCard from "./ProfileCard";
+import { Session } from "../session-store";
 
 import styles from "./css/Profile.module.css";
+import axios from "axios";
 
 type ProfileProps = {
   imageSrc: any;
-  verifiedEmail: string;
 };
 
 export default function Profile({
   imageSrc = "/profile_bear.png",
-  verifiedEmail = "myl39@cornell.edu",
 }: ProfileProps) {
+  const [verifiedEmail, setVerifiedEmail] = useState("");
+
+  async function getVerifiedEmail() {
+    const response = await axios.post("/v2/getStudentEmailByToken", {
+      token: Session.get("token"),
+    });
+
+    const res = response.data.result;
+    console.log(res);
+    if (res.code === 200) {
+      console.log(res.message);
+      setVerifiedEmail(res.message);
+    }
+  }
+
+  getVerifiedEmail();
+  console.log("hello" + verifiedEmail);
   return (
     <div className={styles.profileContainer}>
       <div className={styles.profileTitle}>Profile</div>
       <div className={styles.profileInfo}>
-        <img src={imageSrc} alt='user' />
+        <img src={imageSrc} alt="user" />
         <div className={styles.profileVerifiedEmail}>
           Verified as: {verifiedEmail}
         </div>
         <div className={styles.profileUserStatisticsText}>User Statistics</div>
         <div className={styles.profileUserStatistics}>
           <ProfileCard
-            title='Reviews Total'
-            value='12'
-            image='/total_reviews_icon.svg'
+            title="Reviews Total"
+            value="12"
+            image="/total_reviews_icon.svg"
           />
           <ProfileCard
-            title='People found your reviews helpful'
-            value='7'
-            image='/helpful_review_icon.svg'
+            title="People found your reviews helpful"
+            value="7"
+            image="/helpful_review_icon.svg"
           ></ProfileCard>
           <ProfileCard
-            title='Views Total'
-            value='12'
-            image='/views_icon.svg'
+            title="Views Total"
+            value="12"
+            image="/views_icon.svg"
           ></ProfileCard>
         </div>
         <button className={styles.profileSignOutButton}>
