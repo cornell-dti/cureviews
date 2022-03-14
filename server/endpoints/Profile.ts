ef515115d59dbbc6b68772a07e59285c7276d8b6
jack-farley <jfarley872@gmail.com>
2022-03-13 20:07:05 -0400

    build fix

--- server/endpoints/Profile.ts
+++ server/endpoints/Profile.ts
@@ -82,7 +82,7 @@ async function getreviewIDsByStudentID(netId: string) {
   const studentDoc = await Students.findOne({ netId });
   const reviewIds = studentDoc.reviews;
   const reviews: ReviewDocument[] = await Promise.all(
-    (reviewIds).map(async (reviewId) => await Reviews.findOne({ _id: reviewId });),
+    reviewIds.map(async reviewId => await Reviews.findOne({ _id: reviewId })),
   );
   return reviews;
 }
