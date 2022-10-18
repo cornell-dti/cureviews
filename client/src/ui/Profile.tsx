e9fd3e67db1499434d2f0d4a68ebe782bd071205
jack-farley <jfarley872@gmail.com>
2022-10-18 17:28:37 -0400

    removing unused function

--- client/src/ui/Profile.tsx
+++ client/src/ui/Profile.tsx
@@ -102,21 +102,6 @@ export default function Profile(imgSrc: any) {
   getReviewsTotal();
   getReviewsHelpful();
 
-  const profilePictures = [
-    "/profile_bear/profile_bear_dark_blue.svg",
-    "/profile_bear/profile_bear_light_blue.svg",
-    "/profile_bear/profile_bear_light_pink.svg",
-    "/profile_bear/profile_bear_mint.png",
-    "/profile_bear/profile_bear_orange.svg",
-    "/profile_bear/profile_bear_purple.svg",
-    "/profile_bear/profile_bear_red.svg",
-    "/profile_bear/profile_bear_yellow.svg",
-  ];
-
-  function randomPicture() {
-    return profilePictures[Math.floor(Math.random() * profilePictures.length)];
-  }
-
   if (!loading && isLoggedIn) {
     return (
       <div className={`row ${styles.fullScreen}`}>
