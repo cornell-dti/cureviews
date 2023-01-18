c648bd5354b85730ce488f5a3a939d1b356e51ca
Michelle Li <michelleyli123@gmail.com>
2023-01-18 18:22:48 -0500

    hash function

--- client/src/util/profile_picture.ts
+++ client/src/util/profile_picture.ts
@@ -1,26 +1,22 @@
-
-
 const profilePictures = [
-    "/profile_bear/profile_bear_dark_blue.svg",
-    "/profile_bear/profile_bear_light_blue.svg",
-    "/profile_bear/profile_bear_light_pink.svg",
-    "/profile_bear/profile_bear_mint.png",
-    "/profile_bear/profile_bear_orange.svg",
-    "/profile_bear/profile_bear_purple.svg",
-    "/profile_bear/profile_bear_red.svg",
-    "/profile_bear/profile_bear_yellow.svg",
+  "/profile_bear/profile_bear_dark_blue.svg",
+  "/profile_bear/profile_bear_light_blue.svg",
+  "/profile_bear/profile_bear_light_pink.svg",
+  "/profile_bear/profile_bear_mint.png",
+  "/profile_bear/profile_bear_orange.svg",
+  "/profile_bear/profile_bear_purple.svg",
+  "/profile_bear/profile_bear_red.svg",
+  "/profile_bear/profile_bear_yellow.svg",
 ];
 
-function hashCode(netId: string){
-    var hash = 0;
-    for (var i = 0; i < netId.length; i++) {
-        var code = netId.charCodeAt(i);
-        hash = ((hash<<5)-hash)+code;
-        hash = hash & hash;
-    }
-    return hash;
+function hashCode(netId: string) {
+  var h = 0,
+    l = netId.length,
+    i = 0;
+  if (l > 0) while (i < l) h = ((h << 5) - h + netId.charCodeAt(i++)) | 0;
+  return h;
 }
 
-export function randomPicture(netId : string) {
-    return profilePictures[hashCode(netId)%profilePictures.length];
+export function randomPicture(netId: string) {
+  return profilePictures[hashCode(netId) % profilePictures.length];
 }
