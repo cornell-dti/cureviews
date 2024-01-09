aeae0dd1c75bcc1b3b759ec34f2abeaa1dbcf2fe
Michelle Li <michelleyli123@gmail.com>
2024-01-09 15:24:03 -0500

    levenshtein

--- server/src/search/search.algo.ts
+++ server/src/search/search.algo.ts
@@ -17,17 +17,17 @@ const editDistance = (a, b) => {
   }
 
   const matrix = new Array<number[]>(len_b + 1);
-  for (let i = 0; i <= len_b; ++i) {
+
+  for (let i = 0; i <= len_b; i++) {
     let row = (matrix[i] = new Array<number>(len_a + 1));
     row[0] = i;
   }
-  const firstRow = matrix[0];
-  for (let j = 1; j <= len_a; ++j) {
-    firstRow[j] = j;
+  for (let j = 1; j <= len_a; j++) {
+    matrix[0][j] = j;
   }
 
-  for (let i = 1; i <= len_b; ++i) {
-    for (let j = 1; j <= len_a; ++j) {
+  for (let i = 1; i <= len_b; i++) {
+    for (let j = 1; j <= len_a; j++) {
       if (b.charAt(i - 1) === a.charAt(j - 1)) {
         matrix[i][j] = matrix[i - 1][j - 1];
       } else {
