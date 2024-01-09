a8a7d6ec11a6894de5bc1502f886e191649fc1b6
Michelle Li <michelleyli123@gmail.com>
2024-01-09 15:22:11 -0500

    levenshtein algorithm

--- server/src/search/search.algo.ts
+++ server/src/search/search.algo.ts
@@ -6,40 +6,42 @@
 // uses levenshtein algorithm to return the minimum edit distance between two strings.
 // It is exposed here for testing
 const editDistance = (a, b) => {
-  if (a.length === 0) return b.length;
-  if (b.length === 0) return a.length;
+  const len_a = a ? a.length : 0;
+  const len_b = b ? b.length : 0;
 
-  const matrix = [b.length][a.length];
-
-  // increment along the first column of each row
-  for (let i = 0; i <= b.length; i++) {
-    matrix[i] = [i];
+  if (len_a === 0) {
+    return len_b;
+  }
+  if (len_b === 0) {
+    return len_a;
   }
 
-  // increment each column in the first row
-  let j = 0;
-  for (j = 0; j <= a.length; j++) {
-    matrix[0][j] = j;
+  const matrix = new Array<number[]>(len_b + 1);
+  for (let i = 0; i <= len_b; ++i) {
+    let row = (matrix[i] = new Array<number>(len_a + 1));
+    row[0] = i;
+  }
+  const firstRow = matrix[0];
+  for (let j = 1; j <= len_a; ++j) {
+    firstRow[j] = j;
   }
 
-  // Fill in the rest of the matrix
-  for (let i = 1; i <= b.length; i++) {
-    for (j = 1; j <= a.length; j++) {
+  for (let i = 1; i <= len_b; ++i) {
+    for (let j = 1; j <= len_a; ++j) {
       if (b.charAt(i - 1) === a.charAt(j - 1)) {
         matrix[i][j] = matrix[i - 1][j - 1];
       } else {
-        matrix[i][j] = Math.min(
-          matrix[i - 1][j - 1] + 1, // substitution
+        matrix[i][j] =
           Math.min(
-            matrix[i][j - 1] + 1, // insertion
-            matrix[i - 1][j] + 1,
-          ),
-        ); // deletion
+            matrix[i - 1][j - 1], // substitution
+            matrix[i][j - 1], // insertion
+            matrix[i - 1][j], // deletion
+          ) + 1;
       }
     }
   }
 
-  return matrix[b.length][a.length];
+  return matrix[len_b][len_a];
 };
 
 // a wrapper for a comparator function to be used to sort courses by comparing their edit distance with the query
