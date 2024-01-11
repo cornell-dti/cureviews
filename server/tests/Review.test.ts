a9aac1963eac5d5b589dd8d6c6aed4e912b220d5
Michelle Li <michelleyli123@gmail.com>
2024-01-11 13:02:55 -0500

    review tests

--- server/tests/Review.test.ts
+++ server/tests/Review.test.ts
@@ -4,16 +4,11 @@ import axios from 'axios';
 import { Reviews, Students } from '../db/schema';
 import { testClasses, testReviews, testStudents } from './mocks/InitMockDb';
 import { testServer, testPort } from './mocks/MockServer';
-import { validTokenPayload } from './mocks/MockAuth';
-
-import { Auth } from '../src/auth/auth';
+import {
+  mockVerificationTicket,
+  getValidTokenMock,
+} from './mocks/MockAuthFunctions';
 import { Review } from 'common';
-
-const mockVerificationTicket = jest
-  .spyOn(Auth.prototype, 'getVerificationTicket')
-  .mockImplementation(async () => {
-    return validTokenPayload;
-  });
 
 beforeAll(async () => {
   // get mongoose all set up
@@ -29,6 +24,7 @@ beforeAll(async () => {
 afterAll(async () => {
   // await mockVerifyToken.mockRestore();
   await mockVerificationTicket.mockRestore();
+  await getValidTokenMock.mockRestore();
   await testServer.shutdownTestingServer();
 });
 
@@ -51,7 +47,7 @@ describe('review functionality unit tests', () => {
       {
         courseId: 'oH37S3mJ4eAsktypy',
         review: reviewToInsert,
-        token: 'testToken',
+        token: 'fakeTokenDti1',
       },
     );
     expect(res.status).toBe(200);
@@ -68,7 +64,7 @@ describe('review functionality unit tests', () => {
   it('like/dislike - increment and decrement', async () => {
     const res1 = await axios.post(
       `http://localhost:${testPort}/api/updateLiked`,
-      { id: '4Y8k7DnX3PLNdwRPr', token: 'testToken' },
+      { id: '4Y8k7DnX3PLNdwRPr', token: 'fakeTokenDti1' },
     );
 
     expect(res1.status).toBe(200);
@@ -77,43 +73,11 @@ describe('review functionality unit tests', () => {
 
     const res2 = await axios.post(
       `http://localhost:${testPort}/api/updateLiked`,
-      { id: '4Y8k7DnX3PLNdwRPr', token: 'testToken' },
+      { id: '4Y8k7DnX3PLNdwRPr', token: 'fakeTokenDti1' },
     );
 
     const reviewDisliked = await Reviews.findOne({ _id: '4Y8k7DnX3PLNdwRPr' });
     expect(res2.status).toBe(200);
     expect(reviewDisliked?.likes).toBe(2);
   });
-
-  // it('insertUser', async () => {
-  //   const user1 = {
-  //     _id: 'Irrelevant',
-  //     firstName: 'Cornellius',
-  //     lastName: 'Vanderbilt',
-  //     netId: 'cv4620',
-  //     affiliation: null,
-  //     token: 'fakeTokencv4620',
-  //     privilege: 'regular',
-  //   };
-
-  //   const gObj1 = {
-  //     email: 'cv4620@cornell.edu',
-  //     // This is the google people, not us. It has to be this way:
-  //     // https://googleapis.dev/nodejs/google-auth-library/latest/interfaces/TokenPayload.html
-  //     // eslint-disable-next-line @typescript-eslint/camelcase
-  //     given_name: user1.firstName,
-  //     // eslint-disable-next-line  @typescript-eslint/camelcase
-  //     family_name: user1.lastName,
-  //   };
-
-  //   const res = await axios.post(
-  //     `http://localhost:${testPort}/api/insertUser`,
-  //     { googleObject: gObj1 },
-  //   );
-  //   expect(res.data.result).toBe(1);
-  //   expect(
-  //     (await Students.find({}).exec()).filter((s) => s.netId === 'cv4620')
-  //       .length,
-  //   ).toBe(1);
-  // });
 });
