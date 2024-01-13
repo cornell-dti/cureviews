ca4231d3b3e09739e5590f216b70f66ff22579bc
Michelle Li <michelleyli123@gmail.com>
2024-01-12 22:06:06 -0500

    fixing null error

--- server/src/profile/profile.router.ts
+++ server/src/profile/profile.router.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
-import { Profile } from "./profile";
+import { Profile } from './profile';
 
 import {
   getStudentReviewDocs,
   getTotalLikesByNetId,
-} from "./profile.controller";
-import { ProfileInfoRequestType } from "./profile.type";
+} from './profile.controller';
+import { ProfileInfoRequestType } from './profile.type';
 
 export const profileRouter = express.Router();
 
@@ -14,7 +14,7 @@ export const profileRouter = express.Router();
  * Counts the number of reviews made by a given student id.
  */
 
-profileRouter.post("/countReviewsByStudentId", async (req, res) => {
+profileRouter.post('/countReviewsByStudentId', async (req, res) => {
   try {
     const { netId }: ProfileInfoRequestType = req.body;
     const profile: Profile = new Profile({ netId });
@@ -23,9 +23,7 @@ profileRouter.post("/countReviewsByStudentId", async (req, res) => {
     const studentReviewIds = await getStudentReviewDocs({ netId: validNetId });
 
     if (studentReviewIds === null || studentReviewIds.length === 0) {
-      return res
-        .status(404)
-        .json({ error: "No reviews objects were associated." });
+      return res.status(200).json({ result: 0 });
     }
 
     return res.status(200).json({ result: studentReviewIds.length });
@@ -40,7 +38,7 @@ profileRouter.post("/countReviewsByStudentId", async (req, res) => {
  * [getTotalLikesByStudentId] returns the total number of likes a student has gotten on their reviews
  */
 
-profileRouter.post("/getTotalLikesByStudentId", async (req, res) => {
+profileRouter.post('/getTotalLikesByStudentId', async (req, res) => {
   try {
     const { netId }: ProfileInfoRequestType = req.body;
     const profile: Profile = new Profile({ netId });
@@ -67,13 +65,16 @@ profileRouter.post("/getTotalLikesByStudentId", async (req, res) => {
 /**
  * [getReviewsByStudentId] returns a list of review objects that are created by the given student's netID
  */
-profileRouter.post("/getReviewsByStudentId", async (req, res) => {
+profileRouter.post('/getReviewsByStudentId', async (req, res) => {
   try {
     const { netId }: ProfileInfoRequestType = req.body;
     const profile: Profile = new Profile({ netId });
 
     const validNetId: string = profile.getNetId();
     const reviews = await getStudentReviewDocs({ netId: validNetId });
+    if (!reviews) {
+      return res.status(200).json({ result: [] });
+    }
 
     return res.status(200).json({ result: reviews });
   } catch (err) {
