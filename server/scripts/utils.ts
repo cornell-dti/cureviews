b798c8eed0f2ed857388a919999a3b4299199bb0
Michelle Li <michelleyli123@gmail.com>
2024-01-13 04:45:44 -0500

    uils documentation

--- server/scripts/utils.ts
+++ server/scripts/utils.ts
@@ -1,8 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
+/**
+ * Fetches all available semesters in class roster
+ *
+ * @returns list of semester strings on success, or empty list if failed
+ */
 export async function findAllSemesters(): Promise<string[]> {
   let response = await axios.get(
-    "https://classes.cornell.edu/api/2.0/config/rosters.json",
+    'https://classes.cornell.edu/api/2.0/config/rosters.json',
     { timeout: 30000 },
   );
   if (response.status !== 200) {
