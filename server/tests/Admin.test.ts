ee32726c7f3d38fd4f92375eedd3f7ff8380bf09
Michelle Li <michelleyli123@gmail.com>
2024-01-12 20:30:20 -0500

    make sure class metrics are being updated correctly

--- server/tests/Admin.test.ts
+++ server/tests/Admin.test.ts
@@ -1,6 +1,7 @@
+/* eslint-disable operator-linebreak */
 import axios from 'axios';
 
-import { Reviews } from '../db/schema';
+import { Classes, Reviews } from '../db/schema';
 import { testServer, testPort } from './mocks/MockServer';
 import * as AdminAuth from '../src/admin/admin.controller';
 import { testClasses, testReviews } from './mocks/InitMockDb';
@@ -8,6 +9,23 @@ import { testClasses, testReviews } from './mocks/InitMockDb';
 const mockVerification = jest
   .spyOn(AdminAuth, 'verifyTokenAdmin')
   .mockImplementation(async ({ auth }) => true);
+
+const getReviewDifficultyMetric = async (review) => {
+  const reviews = await Reviews.find({
+    class: review?.class,
+    visible: 1,
+    reported: 0,
+  });
+
+  let totalSum = 0;
+  reviews.map((r) => {
+    totalSum += r.difficulty ? r.difficulty : 0;
+    return totalSum;
+  });
+
+  const avg = reviews.length !== 0 ? totalSum / reviews.length : null;
+  return avg;
+};
 
 beforeAll(async () => {
   await testServer.setUpDB(
@@ -65,10 +83,15 @@ describe('admin functionality unit tests', () => {
     expect(res.status).toEqual(200);
     const review = await Reviews.findOne({ _id: pendingReview?._id });
     expect(review?.visible).toEqual(1);
+
+    const reviewClass = await Classes.findById(pendingReview?.class);
+    const avg = await getReviewDifficultyMetric(pendingReview);
+    expect(reviewClass?.classDifficulty).toEqual(avg);
   });
 
   it('undoReportReview-works', async () => {
     const reportedReview = await Reviews.findOne({ visible: 0, reported: 1 });
+
     const res = await axios.post(
       `http://localhost:${testPort}/api/undoReportReview`,
       {
@@ -81,6 +104,10 @@ describe('admin functionality unit tests', () => {
     const reviewFromDb = await Reviews.findById(reportedReview?._id).exec();
     expect(reviewFromDb?.visible).toEqual(1);
     expect(reviewFromDb?.reported).toEqual(0);
+
+    const reviewClass = await Classes.findById(reportedReview?.class);
+    const avg = await getReviewDifficultyMetric(reportedReview);
+    expect(reviewClass?.classDifficulty).toEqual(avg);
   });
 
   it('removeReview-works', async () => {
@@ -91,12 +118,13 @@ describe('admin functionality unit tests', () => {
         review: reportedReview,
         token: 'non empty',
       })
-      .catch((e) => {
-        console.log(e);
-        return e;
-      });
+      .catch((e) => e);
     expect(res.status).toEqual(200);
     const review = await Reviews.findById(reportedReview?._id);
     expect(review).toEqual(null);
+
+    const reviewClass = await Classes.findById(reportedReview?.class);
+    const avg = await getReviewDifficultyMetric(reportedReview);
+    expect(reviewClass?.classDifficulty).toEqual(avg);
   });
 });
