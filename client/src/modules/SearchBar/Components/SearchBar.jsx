9dae03401dc232ed7de18c5dd047be0aeb1777ba
Michelle Li <michelleyli123@gmail.com>
2024-01-16 14:27:29 -0500

    fix search bar css

--- client/src/modules/SearchBar/Components/SearchBar.jsx
+++ client/src/modules/SearchBar/Components/SearchBar.jsx
@@ -373,8 +373,7 @@ export class SearchBar extends Component {
       >
         <div
           className={
-            'col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 searchbar ' +
-            (this.props.isInNavbar ? 'searchbar-in-navbar' : '')
+            'searchbar ' + (this.props.isInNavbar ? 'searchbar-in-navbar' : '')
           }
         >
           <input
