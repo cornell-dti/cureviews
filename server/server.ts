f816ab2e835deba961412d4810d9a9bbf4620248
Michelle Li <michelleyli123@gmail.com>
2024-01-21 11:18:22 -0500

    minor comment

--- server/server.ts
+++ server/server.ts
@@ -1,20 +1,27 @@
-import path from "path";
-import express from "express";
-import sslRedirect from "heroku-ssl-redirect";
-import cors from "cors";
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable function-paren-newline */
+import path from 'path';
+import express from 'express';
+import sslRedirect from 'heroku-ssl-redirect';
+import cors from 'cors';
 
-import { setupDb } from "./db";
-import { configure } from "./endpoints";
+import { setupDb } from './db';
+import { configure } from './endpoints';
 
 const app = express();
-app.use(sslRedirect(["development", "production"]));
+app.use(sslRedirect(['development', 'production']));
+
+// technically not needed since we proxy calls?
 app.use(cors());
-app.use(express.static(path.join(__dirname, "../../client/build")));
+
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 function setup() {
   const port = process.env.PORT || 8080;
 
-  app.get("*", (_, response) => response.sendFile(path.join(__dirname, "../../client/build/index.html")));
+  app.get('*', (_, response) =>
+    response.sendFile(path.join(__dirname, '../../client/build/index.html')),
+  );
 
   app.use(express.json());
 
