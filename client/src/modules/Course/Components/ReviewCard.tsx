18b99e7e1d958e2a8a61de08b8b3c035d3a0c15d
Will Zhang <wz282@cornell.edu>
2024-02-04 16:24:13 -0500

    old styling + ex vars rm

--- client/src/modules/Course/Components/ReviewCard.tsx
+++ client/src/modules/Course/Components/ReviewCard.tsx
@@ -37,22 +37,13 @@ export default function ReviewCard({
   const location = useLocation()
 
   const [_review, setReview] = useState<ReviewType>(review)
-  const [expanded, setExpanded] = useState<boolean>(false)
-  const [height, setHeight] = useState<number>(isPreview ? 206 : 196)
   const [liked, setLiked] = useState<boolean>(false)
 
   const [courseTitle, setCourseTitle] = useState<string>('')
   const [courseSub, setCourseSub] = useState<string>('')
   const [courseNum, setCourseNum] = useState<string>('')
 
-  const review_container_style = _review.visible
-    ? styles.reviewContainerStyle
-    : styles.reviewContainerStylePending
-  const ratings_container_color = _review.visible
-    ? styles.ratingsContainerColor
-    : ''
-
-  function getDateString() {
+  function dateToString() {
     if (!_review.date) return ''
 
     const date = new Date(_review.date)
@@ -61,18 +52,6 @@ export default function ReviewCard({
     let review_day = date.getDate()
 
     return review_month + '/' + review_day + '/' + review_year
-  }
-
-  function executeOnClick() {
-    if (!expanded) {
-      let newHeight =
-        height + ((_review.text.length % 500) / 20) * (isPreview ? 4.25 : 10.25)
-      setExpanded(!expanded)
-      setHeight(newHeight)
-    } else {
-      setExpanded(!expanded)
-      setHeight(isPreview ? 206 : 196)
-    }
   }
 
   /**
@@ -137,8 +116,8 @@ export default function ReviewCard({
     if (isProfile) {
       return (
         <>
-          <h5 className={styles.courseTitle}>{courseTitle}</h5>
-          <p className={styles.courseCodeAndProf}>
+          <h5>{courseTitle}</h5>
+          <p>
             {courseSub?.toUpperCase() +
               ' ' +
               courseNum?.toUpperCase() +
@@ -231,7 +210,7 @@ export default function ReviewCard({
           </button>
         )}
         <div className={styles.datehelpful}>
-          <div> {getDateString()} </div>
+          <div> {dateToString()} </div>
           {!isPreview && (
             <div
               className={`${styles.helpful} ${liked && styles.likedhelpful}`}
@@ -255,145 +234,4 @@ export default function ReviewCard({
       </div>
     </div>
   )
-
-  // return (
-  //   <div className={styles.reviewContainer + ' ' + review_container_style}>
-  //     {/* Flag */}
-  //     {!isPreview && (
-  //       <div className={styles.buttonContainer}>
-  //         <div className={styles.flagContainer}>
-  //           <button
-  //             onClick={() => {
-  //               reportHandler(_review)
-  //               alert('This post has been reported and will be reviewed.')
-  //             }}
-  //           >
-  //             <img src="/report-flag.svg" alt="report review button"></img>
-  //           </button>
-  //         </div>
-  //         <div className={styles.pencilContainer}>
-  //           <button
-  //             onClick={() => {
-  //               alert('Editing is currently not available, stay tuned!')
-  //             }}
-  //           >
-  //             <img src="/pencil.svg" alt="edit review button"></img>
-  //           </button>
-  //         </div>
-  //       </div>
-  //     )}
-
-  //     {/* Main Section */}
-  //     <div className={`${styles.reviewMainSectionContainer}`}>
-  //       {/* Ratings section. */}
-  //       <div className={styles.ratingsSection}>
-  //         <div
-  //           className={styles.ratingsContainer + ' ' + ratings_container_color}
-  //         >
-  //           <div className={styles.ratingElem}>
-  //             <span>Overall</span>
-  //             <span className={styles.ratingNum}>
-  //               {_review.rating ? _review.rating : '-'}
-  //             </span>
-  //           </div>
-  //           <div className={styles.ratingElem}>
-  //             <span>Difficulty</span>
-  //             <span className={styles.ratingNum}>
-  //               {_review.difficulty ? _review.difficulty : '-'}
-  //             </span>
-  //           </div>
-  //           <div className={styles.ratingElem}>
-  //             <span>Workload</span>
-  //             <span className={styles.ratingNum}>
-  //               {_review.workload ? _review.workload : '-'}
-  //             </span>
-  //           </div>
-  //         </div>
-  //       </div>
-
-  //       {/* Title, professor, review */}
-  //       <div className="">
-  //         <div className={styles.contentContainer}>
-  //           {/* Title And Professor */}
-  //           <TitleAndProfessor></TitleAndProfessor>
-
-  //           <div className={styles.gradeMajorContainer}>
-  //             <div>
-  //               <span>Grade: </span>
-  //               {_review.grade &&
-  //               _review.grade.length !== 0 &&
-  //               /^([^0-9]*)$/.test(_review.grade) ? (
-  //                 <span className={styles.gradeMajorText}>{_review.grade}</span>
-  //               ) : (
-  //                 <span className={styles.gradeMajorText}>N/A</span>
-  //               )}
-  //             </div>
-  //             <div>
-  //               <span>Major(s): </span>
-  //               {_review.major && _review.major.length !== 0 ? (
-  //                 _review.major.map((major, index) => (
-  //                   <span className={styles.gradeMajorText} key={index}>
-  //                     {index > 0 ? ', ' : ''}
-  //                     {major}
-  //                   </span>
-  //                 ))
-  //               ) : (
-  //                 <span className={styles.gradeMajorText}>N/A</span>
-  //               )}
-  //             </div>
-  //           </div>
-
-  //           {/* Review Text */}
-  //           <p className={styles.reviewText}>
-  //             <ShowMoreText
-  //               lines={3}
-  //               more="Show more"
-  //               less="Show less"
-  //               anchorClass="showMoreText"
-  //               onClick={executeOnClick}
-  //               expanded={expanded}
-  //             >
-  //               {_review.text}
-  //             </ShowMoreText>
-  //           </p>
-
-  //           {/* Date, Like Button*/}
-  //           <div className="">
-  //             <p className={styles.date}>{getDateString()}</p>
-
-  //             {/* Like Button */}
-  //             {!isPreview && (
-  //               <div>
-  //                 <button
-  //                   className={
-  //                     liked === true ? 'review-voted' : 'review-upvote'
-  //                   }
-  //                   onClick={() => {
-  //                     increment()
-  //                   }}
-  //                 >
-  //                   <img
-  //                     src={liked ? '/handClap_liked.svg' : '/handClap.svg'}
-  //                     alt={liked ? 'Liked' : 'Not Liked Yet'}
-  //                   />
-
-  //                   <p className={styles.upvoteText}>
-  //                     Helpful ({_review.likes ? _review.likes : 0})
-  //                   </p>
-  //                 </button>
-  //               </div>
-  //             )}
-  //           </div>
-
-  //           {_review.isCovid && (
-  //             <div className={styles.covidTag}>
-  //               <span role="img" aria-label="alert"></span>
-  //               This student's experience was affected by COVID-19
-  //             </div>
-  //           )}
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // )
 }
