4322801034dc084f08175516d2f8896c38e5be7b
Will Zhang <wz282@cornell.edu>
2024-02-19 01:04:32 -0500

    rm old styles, unused css, and source sans font + fix write icon import

--- client/src/modules/Course/Components/Course.tsx
+++ client/src/modules/Course/Components/Course.tsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import WriteReviewIcon from '../../../assets/icons/write.svg'
+import WriteReviewIcon from '../../../assets/icons/writeicon.svg'
 
 import { courseVisited } from './Feedback'
 import Navbar from '../../Globals/Navbar'
