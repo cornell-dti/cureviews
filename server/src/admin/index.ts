ab396d4c5a197a09c73de53f3fc286eb6ff1acff
Will Zhang <wz282@cornell.edu>
2024-05-01 14:06:04 -0400

    add typescript 5.4^ to server dev dep to allow tsc

--- server/src/admin/index.ts
+++ server/src/admin/index.ts
@@ -1,1 +1,22 @@
 export { adminRouter } from "./admin.router";
+
+
+import OpenAI from "openai";
+
+// const openai = new OpenAI();
+
+// async function complete() {
+//   const completion = await openai.chat.completions.create({
+//     model: 'gpt-3.5-turbo',
+//     messages: [
+//       {
+//         role: 'system', content: 'You are creating a 50 word summary based.'
+//       },
+//       {
+//         role: 'user', content: 'hi'
+//       }
+//     ]
+//   })
+
+//   return completion.choices[0].message.content;
+// }
