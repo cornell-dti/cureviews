788adec7454f3bed87317eec5847fe3bdbb9b5a7
qiandrewj <qiandrewj@gmail.com>
2024-09-21 22:57:09 -0400

    updating to eslint with vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import svgr from 'vite-plugin-svgr';
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
     build: {
@@ -8,7 +8,7 @@ export default defineConfig({
     },
     plugins: [
       react(),
-      // svgr({ svgrOptions: { icon: true } }),
+      eslint()
     ],
     server: {
       port: 3000,
