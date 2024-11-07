cde7da5fa77914f1e12a5bed370e65e537647078
Andrew Qian <qiandrewj@gmail.com>
2024-11-06 22:23:40 -0500

    returned endpoint to backend

--- server/src/profile/profile.router.ts
+++ server/src/profile/profile.router.ts
@@ -10,6 +10,30 @@ import { ProfileInfoRequestType } from './profile.type';
 
 export const profileRouter = express.Router();
 
+/** Reachable at POST /api/profiles/count-reviews
+ * @body netId: a user's netId
+ * Gets all reviews made by the user with the given netId
+*/
+profileRouter.post('/count-reviews', async (req, res) => {
+  try {
+    const { netId }: ProfileInfoRequestType = req.body;
+    const profile: Profile = new Profile({ netId });
+    const validNetId: string = profile.getNetId();
+
+    const studentReviewIds = await getStudentReviewDocs({ netId: validNetId });
+
+    if (studentReviewIds === null || studentReviewIds.length === 0) {
+      return res.status(200).json({ result: 0 });
+    }
+
+    return res.status(200).json({ result: studentReviewIds.length });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: `Internal Server Error: ${error.message}` });
+  }
+});
+
 /** Reachable at POST /api/profiles/get-likes
  * @body netId: a user's netId
  * Gets the total number of likes by the user with the given netId
