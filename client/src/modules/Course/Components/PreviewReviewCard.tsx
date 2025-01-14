09d7767c8b51915b9a13eb9140c75288273e65e9
mjaydenkim <mjaydenkim@gmail.com>
2025-01-13 19:51:20 -0500

    Sort professor names in review cards

--- client/src/modules/Course/Components/PreviewReviewCard.tsx
+++ client/src/modules/Course/Components/PreviewReviewCard.tsx
@@ -102,7 +102,7 @@ export default function PreviewReviewCard({
     // list of professors (name1, name2, ..)
     let professornames = '';
     if (_review.professors && _review.professors.length > 0)
-      professornames += _review.professors.join(', ');
+      professornames += _review.professors.sort().join(', ');
     else professornames += 'Not Listed';
 
     if (isProfile) {
