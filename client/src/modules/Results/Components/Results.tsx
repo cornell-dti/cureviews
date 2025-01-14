e6b04f75034e806ba589bf24ff893e57c01bc8d2
mjaydenkim <mjaydenkim@gmail.com>
2025-01-14 12:19:40 -0500

    Revert "Added link to box text. Iffy about this one"
    This reverts commit 92a427b7ae54c37268aa130af063d557714ba2bc.

--- client/src/modules/Results/Components/Results.tsx
+++ client/src/modules/Results/Components/Results.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import axios from 'axios';
 
 import Navbar from '../../Globals/Navbar';
