130c659169f4edc8e3a3c30ddb1ea58002d42682
mjaydenkim <mjaydenkim@gmail.com>
2025-04-06 15:58:00 -0400

    Added some charts etc

--- client/src/modules/Course/Components/CourseEval.tsx
+++ client/src/modules/Course/Components/CourseEval.tsx
@@ -1,9 +1,32 @@
 import React, { useEffect, useState } from 'react';
+
 import { CourseEvaluation } from 'common';
+import styles from '../Styles/CourseEval.module.css'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar, Pie } from 'react-chartjs-2';
 
 type CourseEvalProps = {
   courseEval: CourseEvaluation;
 };
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const CourseEval = ({ courseEval }: CourseEvalProps) => {
   const [mappedSentiments, setMappedSentiments] = useState(
@@ -12,38 +35,167 @@ const CourseEval = ({ courseEval }: CourseEvalProps) => {
 
   useEffect(() => {
     setMappedSentiments(mapSentiments(courseEval.sentiments));
-    console.log(mappedSentiments)
+    console.log(mappedSentiments);
   }, [courseEval]);
+
+  const horiz_options = {
+    indexAxis: 'y' as const,
+    elements: {
+      bar: {
+        borderWidth: 2
+      }
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'right' as const
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Horizontal Bar Chart'
+      }
+    }
+  };
+
+  const vert_options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart'
+      }
+    }
+  };
+
+  const profData = {
+    labels: [
+      'Teaching Skills',
+      'Knowledge of Subject Matter',
+      'Classroom Climate',
+      'Overall'
+    ],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [
+          courseEval.profTeachingSkill,
+          courseEval.profKnowledge,
+          courseEval.profClimate,
+          courseEval.profOverall
+        ],
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)'
+      }
+    ]
+  };
+
+  const gradeData = {
+    labels: ['A', 'B', 'C', 'D', 'F', 'S', 'U', 'Declined'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [
+          courseEval.numA,
+          courseEval.numB,
+          courseEval.numC,
+          courseEval.numD,
+          courseEval.numF,
+          courseEval.numS,
+          courseEval.numU,
+          courseEval.numGradeNA
+        ],
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)'
+      }
+    ]
+  };
+
+  const yearData = {
+    labels: ['Freshman', 'Sophomore', 'Junior', 'Senior'],
+    datasets: [
+      {
+        label: '# of Students',
+        data: [
+          courseEval.numFresh,
+          courseEval.numSoph,
+          courseEval.numJr,
+          courseEval.numSr
+        ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  };
+
+  const collegeData = {
+    labels: ['CALS', 'AAP', 'CAS', 'COE', 'Hotel', 'HumEc', 'ILR'],
+    datasets: [
+      {
+        label: '# of Students',
+        data: [
+          courseEval.numAg,
+          courseEval.numArch,
+          courseEval.numArts,
+          courseEval.numEng,
+          courseEval.numHotel,
+          courseEval.numHumec,
+          courseEval.numILR
+        ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  }
 
   return (
     <div>
       <p>COURSE NAME: {courseEval.courseName}</p>
-      <p>EVAL SEMESTER: {courseEval.semester}</p><br/>
-      <p>-- PROFESSOR RATINGS --</p>
-      <p>TEACHING SKILLS: {courseEval.profTeachingSkill}</p>
-      <p>KNOWLEDGE OF SUBJECT MATTER: {courseEval.profKnowledge}</p>
-      <p>CLASSROOM CLIMATE: {courseEval.profClimate}</p>
-      <p>OVERALL: {courseEval.profOverall}</p><br/>
-
-      <p>APPROX. GRADE IN COURSE: A: {courseEval.numA}. B: {courseEval.numB}. C: {courseEval.numC}. D:{' '}
-        {courseEval.numD}. F: {courseEval.numF}. S: {courseEval.numS}. U:{' '}
-        {courseEval.numU}.
-      </p>
-
-      <p>
-        STUDENT YEAR -- FROSH: {courseEval.numFresh}. SOPH: {courseEval.numSoph}.
-        JR: {courseEval.numJr}. SR: {courseEval.numSr}.
-      </p><br/>
-
-      <p>TOP SENTIMENTS: {mappedSentiments[0].join(', ')}.</p>
-      <p>REASONS FOR TAKING COURSE: {mappedSentiments[1].join(', ')}.</p><br/>
-
-      <p>
-        SCHOOL -- CALS: {courseEval.numAg}. AAP: {courseEval.numArch}. CAS:{' '}
-        {courseEval.numArts}. COE: {courseEval.numEng}. HOTEL:{' '}
-        {courseEval.numHotel}. HUMEC: {courseEval.numHumec}. ILR:{' '}
-        {courseEval.numILR}.
-      </p>
+      <p>EVAL SEMESTER: {courseEval.semester}</p>
+      <div className={styles.dashboardContainer}>
+        <div className={styles.professorRating}>
+          <Bar options={horiz_options} data={profData} />
+        </div>
+        <div className={styles.topSentiments}>
+          <p>TOP SENTIMENTS: {mappedSentiments[0].join(', ')}.</p>
+        </div>
+        <div className={styles.gradeDistribution}>
+          <Bar options={vert_options} data={gradeData} />
+        </div>
+        <div className={styles.studentYear}>
+          <Pie data={yearData} />
+        </div>
+        <div className={styles.schoolCollege}>
+          <Pie data={collegeData} />
+        </div>
+        <div className={styles.reasonTaking}>
+          <p>REASONS FOR TAKING COURSE: {mappedSentiments[1].join(', ')}.</p>
+        </div>
+      </div>
     </div>
   );
 };
